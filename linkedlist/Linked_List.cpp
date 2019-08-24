e6e9f2779aae0194a9f13db5b24304f529ec6ba7
Sammeet <sammeet29@gmai.com>
2019-08-24 15:29:37 -0700

    adding test mocule and fixed linked_list implementation

--- linkedlist/Linked_List.cpp
+++ linkedlist/Linked_List.cpp
@@ -1,111 +1,130 @@
 #include <cstddef>
 #include <cstdlib>
+#include <iostream>
 #include "Linked_List.h"
 
-using namespace std;
 
-// Description	- add node to the start of list
-// Input 		- data of the node to be deleted
-// Return 		- true if successful, else false
-// Output		- None
+Linked_List::Linked_List(){
+    this->head = NULL;
+}
+
+
+// Description  - add node to the start of list
+// Input        - data of the node to be deleted
+// Return       - true if successful, else false
+// Output       - None
 bool Linked_List::add_to_start(int data){
-	// return value
-	bool rtn = true;
+    // return value
+    bool rtn = true;
 
-	Node * new_node =  malloc(sizeof(Node));
+    Node * new_node = (Node *) malloc(sizeof(Node));
 
-	if(new_node == NULL){
-		rtn = false;
-	}
+    if(new_node == NULL){
+        rtn = false;
+    }
 
-	if(rtn){
-		new_node->Data = data;
-		new_node->Next = head;
-		head = new_node;
-	}
+    if(rtn){
+        new_node->Data = data;
+        new_node->Next = head;
+        head = new_node;
+    }
 
-	return (rtn);
+    return (rtn);
 }
 
-// Description	- delete 1st node with data = data
-// Input 		- data of the node to be deleted
-// Return 		- true if successful, else false
-// Output		- Node is deleted from the list 
+// Description  - delete 1st node with data = data
+// Input        - data of the node to be deleted
+// Return       - true if successful, else false
+// Output       - Node is deleted from the list 
 bool Linked_List::delete_node( int data){
-	// return value
-	bool rtn = false;
+    // return value, be a pessimist
+    bool rtn = false;
 
-	Node * prev = head;
-	Node * curr = head;
+    Node * prev = head;
+    Node * curr = head;
 
-	// if we need to delete the head node
-	if((head != NULL) && (head->Data == data)){
-		
-		// rearrange the linked list
-		curr = head;
-		head = head->Next;
+    // if we need to delete the head node
+    if((head != NULL) && (head->Data == data)){
+        
+        // rearrange the linked list
+        curr = head;
+        head = head->Next;
 
-		// skip the loop
-		rtn = true;
+        // skip the loop
+        rtn = true;
 
-		// delete node
-		free(curr);
-	}
+        // delete node
+        free(curr);
+    }
 
-	while ((curr != NULL) && (!rtn)){
+    while ((curr != NULL) && (!rtn)){
 
-		// if this is the node we are looking for
-		// delete the node and rearrange the list
-		if( curr->Data = data){ 
-			rtn = true;
-			prev->next = curr->Next;
-			free(curr);
-		}
+        // if this is the node we are looking for
+        // delete the node and rearrange the list
+        if( curr->Data = data){ 
+            rtn = true;
+            prev->Next = curr->Next;
+            free(curr);
+        }
 
-		// advance loop
-		prev = curr;
-		curr = curr->next;
-	}
+        // advance loop
+        prev = curr;
+        curr = curr->Next;
+    }
 
-	return (rtn);
+    return (rtn);
 }
 
-// Description	- tells if the data is present in the list
-// Return 		- true if node is present, else false
-// Output		- None
+// Description  - tells if the data is present in the list
+// Return       - true if node is present, else false
+// Output       - None
 bool Linked_List::is_present(int data){
-	// return value
-	bool rtn = false;
+    // return value
+    bool rtn = false;
+    Node * curr = this->head;
+    while ( !rtn && (curr != NULL) ){
 
-	while ( !rtn && curr != NULL ){
+        if(curr->Data == data){
+            // exit
+            rtn = true;
+        }
 
-		if(curr->Data == data){
-			// exit
-			rtn = true;
-		}
+        //advance loop
+        curr = curr->Next;
+    }
 
-		//advance loop
-		curr = curr->Next;
-	}
-
-	return(rtn);
+    return(rtn);
 }
 
-// Description	- Prints out the data in list in a line
-// Return 		- none
-// Output		- prints out the list data
+// Description  - Prints out the data in list in a line
+// Return       - none
+// Output       - prints out the list data
 void Linked_List::print_list(){
-	if(head == NULL){
-		return;
-	}
+    if(head == NULL){
+        return;
+    }
 
-	Node * curr = head;
+    Node * curr = head;
 
-	while (curr != NULL){
+    while (curr != NULL){
 
-		cout << curr->Data << " ";
-		curr = curr->Next;
-	}
+        std::cout << curr->Data << " ";
+        curr = curr->Next;
+    }
 
-	cout << endl;
+    std::cout << std::endl;
 }
+
+// Description  - Tells us if the list is empty
+// Return       - True if empty, else false
+// Output       - None
+bool Linked_List::is_empty(){
+    bool rtn = false;
+
+    if(this->head == NULL){
+        rtn = true;
+    }
+
+    return (rtn);
+}
+
